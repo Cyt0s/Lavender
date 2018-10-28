77e74864e016c1711a5f99693fa5fcae0d214302
Cyt0s <eylon.ilouz1@gmail.com>
2018-10-28 16:21:46 +0200

    Change names of methods

--- lavender/hdfs.py
+++ lavender/hdfs.py
@@ -5,27 +5,27 @@ class HDFS(object):
         self.URI = sc._gateway.jvm.java.net.URI
         self.Path = sc._gateway.jvm.org.apache.hadoop.fs.Path
         self.FileSystem = sc._gateway.jvm.org.apache.hadoop.fs.FileSystem
-        self.fs = self.FileSystem.get(self.URI(namenode_uri), sc._jsc.hadoopConfiguration())
+        self.FileUtil = sc._gateway.jvm.org.apache.fs.FileUtil
+        self.conf = sc._jsc.hadoopConfiguration()
+        self.fs = self.FileSystem.get(self.URI(namenode_uri), self.conf)
 
-
-    def list_dir(self, dir_to_list):
+    def ls(self, dir_to_list):
         return self.fs.listStatus(self.Path(dir_to_list))
-
 
     def rm(self, dir_to_remove):
         return self.fs.delete(self.Path(dir_to_remove))
 
-
-    def create_dir(self,dir_to_create):
+    def mkdir(self, dir_to_create):
         return self.fs.mkdirs(self.Path(dir_to_create))
 
-
-    def is_dir_exists(self,dir_to_check):
+    def does_dir_exist(self, dir_to_check):
         return self.fs.exists(self.Path(dir_to_check))
 
-
-    def move(self, src_dir, dest_dir):
+    def mv(self, src_dir, dest_dir):
         return self.fs.rename(self.Path(src_dir), self.Path(dest_dir))
 
-    def get_dir_size(self,dir_to_sample):
+    def get_dir_size(self, dir_to_sample):
         return self.fs.getFileStatus(self.Path(dir_to_sample)).getBlockSize()
+
+    def cp(self, src_dir, dst_dir):
+        return self.FileUtil.copy(self.fs, self.Path(src_dir), self.fs, self.Path(dst_dir), False, self.conf)
